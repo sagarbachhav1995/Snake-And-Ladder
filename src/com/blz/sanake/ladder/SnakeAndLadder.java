c59962ffe30e686b64b3d8c78656a05abf1d84ea
sagarbachhav1995 <sagarbachhav8@gmail.com>
2022-09-13 12:14:56 -0700

    Snake And Ladder

--- src/com/blz/sanake/ladder/SnakeAndLadder.java
+++ src/com/blz/sanake/ladder/SnakeAndLadder.java
@@ -1,12 +1,16 @@
 package com.blz.sanake.ladder;
 
+import java.util.Random;
+
 public class SnakeAndLadder {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		int position = 0;
-	System.out.println("start position :" + position);	
+		Random ran = new Random();
 
+		int dice = ran.nextInt(6) + 1;
+		System.out.println("Dice Outcome : " + dice);
 	}
 
 }
