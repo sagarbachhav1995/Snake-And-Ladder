b7ba381b4657be31d6206ccf85c44bbbf984a296
sagarbachhav1995 <sagarbachhav8@gmail.com>
2022-09-13 13:51:02 -0700

    Snake And Ladder

--- src/com/blz/sanake/ladder/SnakeAndLadder.java
+++ src/com/blz/sanake/ladder/SnakeAndLadder.java
@@ -7,30 +7,42 @@ public class SnakeAndLadder {
 	static final int Snake = 2;
 	static final int Ladder = 1;
 	static final int Noplay = 0;
+	static int Die = 0;
 
-	public static void main(String[] args) {
+	public static void main(String[] args) 
+	{
 		int position = 0;
-		while (position < 100) {
+		System.out.println("position: "+position);
+		
+		while (position<100) 
+		{
 			Random ran = new Random();
-			int dice = ran.nextInt(6) + 1;
-			System.out.println("dice: " + dice);
-
+			int dice = ran.nextInt(6)+1;
+			System.out.println("dice: "+dice);
+			++Die;
 			int optionCheck = ran.nextInt(3);
-			System.out.println("optionCheck: " + optionCheck);
-
-			if ((optionCheck == Ladder) && (position + dice) <= 100) {
-				System.out.println("Ladder");
+			System.out.println("optionCheck: "+optionCheck);
+			
+			
+			if ((optionCheck == Ladder) && (position+dice)<=100) 
+			{
+				System.out.println("ladder");
 				position = position + dice;
-			} else if (optionCheck == Snake) {
+			}
+			else if (optionCheck == Snake) 
+			{
 				System.out.println("Snake");
 				position = position - dice;
-			} else {
-				System.out.println("Noplay");
 			}
-			if (position < 0) {
-				position = 0;
+			else {
+				System.out.println("Noplay");				
 			}
-			System.out.println("position: " + position);
+			if (position<0) 
+			{
+				position =0;
+			}
+			System.out.println("position: "+position+"\ndicecount: "+Die);
+			
 		}
 	}
 
