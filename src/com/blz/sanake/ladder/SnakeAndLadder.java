a5c9981cab288f4111f5d39a533fcc130980d283
sagarbachhav1995 <sagarbachhav8@gmail.com>
2022-09-13 13:45:45 -0700

    Snake And Ladder

--- src/com/blz/sanake/ladder/SnakeAndLadder.java
+++ src/com/blz/sanake/ladder/SnakeAndLadder.java
@@ -8,40 +8,30 @@ public class SnakeAndLadder {
 	static final int Ladder = 1;
 	static final int Noplay = 0;
 
-	public static void main(String[] args) 
-	{
+	public static void main(String[] args) {
 		int position = 0;
-		while (position<100) 
-		{
+		while (position < 100) {
 			Random ran = new Random();
-			int dice = ran.nextInt(6)+1;
-			System.out.println("dice: "+dice);
-			
+			int dice = ran.nextInt(6) + 1;
+			System.out.println("dice: " + dice);
+
 			int optionCheck = ran.nextInt(3);
-			System.out.println("optionCheck: "+optionCheck);
-			
-			
-			if ((optionCheck == Ladder) && (position+dice)<=100) 
-			{
+			System.out.println("optionCheck: " + optionCheck);
+
+			if ((optionCheck == Ladder) && (position + dice) <= 100) {
 				System.out.println("Ladder");
 				position = position + dice;
-			}
-			else if (optionCheck == Snake) 
-			{
+			} else if (optionCheck == Snake) {
 				System.out.println("Snake");
 				position = position - dice;
-			}
-			else 
-			{
+			} else {
 				System.out.println("Noplay");
 			}
-			if (position<0) 
-			{
-				position =0;
+			if (position < 0) {
+				position = 0;
 			}
-			System.out.println("position: "+position);
+			System.out.println("position: " + position);
 		}
 	}
 
 }
- 
