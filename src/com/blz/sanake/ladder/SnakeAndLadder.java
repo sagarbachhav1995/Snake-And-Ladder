e26649e9881491fe0d4af7999715a46df0a1d83f
sagarbachhav1995 <sagarbachhav8@gmail.com>
2022-09-13 13:32:19 -0700

    Snake And Ladder

--- src/com/blz/sanake/ladder/SnakeAndLadder.java
+++ src/com/blz/sanake/ladder/SnakeAndLadder.java
@@ -4,13 +4,38 @@ import java.util.Random;
 
 public class SnakeAndLadder {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+	static final int Snake = 2;
+	static final int ladder = 1;
+	static final int Noplay = 0;
+
+	public static void main(String[] args) 
+	{
 		int position = 0;
 		Random ran = new Random();
-
-		int dice = ran.nextInt(6) + 1;
-		System.out.println("Dice Outcome : " + dice);
+		int dice = ran.nextInt(6)+1;
+		System.out.println("dice: "+dice);
+		
+		int optionCheck = ran.nextInt(3);
+		System.out.println("optionCheck: "+optionCheck);
+		
+		if (optionCheck == ladder) 
+		{
+			System.out.println("Ladder");
+			position = position + dice;
+		}
+		
+		else if (optionCheck == Snake) 
+		{
+			System.out.println("Snake");
+			position = position - dice;
+		}
+		else {
+			System.out.println("No Play");
+		}
+		if (position<0)
+			position=0;
+		System.out.println("position: "+position);
 	}
 
 }
+ 
